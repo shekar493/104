e323b8d99780e00b2b7710ab43c68915e061515f
shekar493 <97027140+shekar493@users.noreply.github.com>
2022-10-25 14:34:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//npm install express
+
+var express = require('express');
+
+var app = express();
+
+// set port, listen for requests
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+
+console.log(`Server is running on port ${PORT}.`);
+
+});
+
+// Default Route – http://localhost:8080
+
+app.get('/', function (req, res) {
+
+res.send('<h1>Hello World</h1>');
+
+})
